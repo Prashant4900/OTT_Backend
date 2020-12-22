6c8c7a45ddcc199e7b0485dfded3ada88ff7bd09
Prashant4900 <62440911+Prashant4900@users.noreply.github.com>
2020-12-22 23:46:32 +0530

    make it better

--- OTT_Backend/settings.py
+++ OTT_Backend/settings.py
@@ -178,3 +178,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
